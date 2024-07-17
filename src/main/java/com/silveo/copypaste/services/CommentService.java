3305e0db375a5c49764530186e2f753350b6dec1
silveoo <92054590+silveoo@users.noreply.github.com>
2024-07-17 15:34:05 +0300

    updated README.md

--- src/main/java/com/silveo/copypaste/services/CommentService.java
+++ src/main/java/com/silveo/copypaste/services/CommentService.java
@@ -5,7 +5,6 @@ import com.silveo.copypaste.repositories.CommentRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 @AllArgsConstructor
